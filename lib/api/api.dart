d6776f69b63e97a83ffb0e1afb6d25f4527ca448
JiletHakan <95987406+JiletHakan@users.noreply.github.com>
2024-05-29 21:51:31 +0300

    api.dart

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -16,9 +16,11 @@ class Api {
   static const _upComingUrl =
       'https://api.themoviedb.org/3/movie/upcoming?api_key=${Constants.apiKey}';
 
+  // Method to fetch trending movies
   Future<List<Movie>> getTrendingMovies() async {
     final response = await http.get(Uri.parse(_trendingUrl));
     if (response.statusCode == 200) {
+      // Decode the JSON response and convert to a list of Movie objects
       final decodedData =
           json.decode(response.body)['results'] as List<dynamic>;
       return decodedData.map((movie) => Movie.fromJson(movie)).toList();
@@ -27,9 +29,11 @@ class Api {
     }
   }
 
+  // Method to fetch top-rated movies
   Future<List<Movie>> getTopRatedMovies() async {
     final response = await http.get(Uri.parse(_topRatedUrl));
     if (response.statusCode == 200) {
+      // Decode the JSON response and convert to a list of Movie objects
       final decodedData =
           json.decode(response.body)['results'] as List<dynamic>;
       return decodedData.map((movie) => Movie.fromJson(movie)).toList();
@@ -38,9 +42,11 @@ class Api {
     }
   }
 
+  // Method to fetch upcoming movies
   Future<List<Movie>> getUpComingMovies() async {
     final response = await http.get(Uri.parse(_upComingUrl));
     if (response.statusCode == 200) {
+      // Decode the JSON response and convert to a list of Movie objects
       final decodedData =
           json.decode(response.body)['results'] as List<dynamic>;
       return decodedData.map((movie) => Movie.fromJson(movie)).toList();
@@ -49,15 +55,20 @@ class Api {
     }
   }
 
+  // Method to fetch reviews from the backend server
   Future<List<Map<String, dynamic>>> getReviews(
       String movieTitleWithoutSpaces, String year) async {
+    // Log the method running state
     _logger.fine(
         'getReviews method is running. Movie title without spaces: $movieTitleWithoutSpaces, Year: $year');
 
+    // Construct the URL for the reviews endpoint
     final url = Uri.parse(
         'http://127.0.0.1:5000/getReviews?name=$movieTitleWithoutSpaces&year=$year');
+    // Send the HTTP GET request to the backend server
     final response = await http.get(url);
     if (response.statusCode == 200) {
+      // Decode the JSON response and convert to a list of maps
       final List<dynamic> reviewsJson = json.decode(response.body);
       return reviewsJson.cast<Map<String, dynamic>>();
     } else {
